e2155064b150e485fa821ba27c121bd3d476f48e
byvanesa <bhansalivanesa@gmail.com>
2022-12-28 10:18:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(Props) {
+        super(Props);
+
+        this.state = {
+            data: {
+                label:["1", "2", "3", "4", "5"],
+                datasets: [
+                {
+                    label: "Temperature",
+                    backgroundColor: "rgba(255, 0, 255, 0.75)",
+                    data: [30, 35, 40, 41, 37, 37, 35 ]
+                },
+                {
+                    label: "Time",
+                    backgroundColor: "rgba(0, 255, 0, 0.75)",
+                    data: [9, 10, 11, 12, 1, 2, 3]
+                }
+            ]
+        }
+    }
+} 
+    render() {
+        return (
+            <div style={{position: "relative", width: 600, height: 550 }}>
+                <h3>Chart Smaple</h3>
+                <Line
+                    options={{
+                        responsive: true
+                    }}
+                    data={this.state.date}
+                />
+            </div>
+        )
+    }
 }
-
-export default App;
